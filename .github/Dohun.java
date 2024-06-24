fda1ce241dace78cf0c4d44108083f4439d25966
sunandrabbit <sunandrabbit@gamil.com>
2024-06-24 15:36:14 +0900

    Feat #4-makeDohunClass

---
+++ .github/Dohun.java
@@ -0,0 +1,26 @@
+public class Dohun {
+
+    private String name;
+    private String birth;
+
+    public Dohun(String name, String birth) {
+        this.name = name;
+        this.birth = birth;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
